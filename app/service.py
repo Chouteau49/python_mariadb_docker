9d155fe69fd85d39c166a6651e61fecb5dba9ab7
Julien <chouteau49@gmail.com>
2024-05-20 02:04:45 +0200

    refactor: add fonctions

--- app/service.py
+++ app/service.py
@@ -18,3 +18,18 @@ class FilmService:
     def get_all_films(self):
         select_query = "SELECT * FROM film"
         return self.repo.select(select_query)
+
+    # delete film by id
+    def delete_film(self, id):
+        delete_query = "DELETE FROM film WHERE id =?"
+        return self.repo.delete(delete_query, (id,))
+
+    # update film by id
+    def update_film(self, id, nom, annee):
+        update_query = "UPDATE film SET nom =?, annee =? WHERE id =?"
+        return self.repo.update(update_query, (nom, annee, id))
+
+    # get film by id
+    def get_film(self, id):
+        select_query = "SELECT * FROM film WHERE id =?"
+        return self.repo.select(select_query, (id,))
